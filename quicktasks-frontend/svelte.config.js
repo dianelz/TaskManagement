6294c449643f8e0911007253aefc3456e759e6b7
Diane Elzaabi <de@port-adhoc.com>
2025-02-27 09:58:27 +0100

    add Hosting V1

--- quicktasks-frontend/svelte.config.js
+++ quicktasks-frontend/svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
